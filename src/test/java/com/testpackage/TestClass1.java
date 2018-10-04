1393664066ddddc4a597cd3fb2fde2c0e0c47e95
Ashish <ashishbajaj@live.com>
2018-10-05 00:03:09 +0530

    Commiting the changes mnade to Testclass1

--- src/test/java/com/testpackage/TestClass1.java
+++ src/test/java/com/testpackage/TestClass1.java
@@ -4,18 +4,18 @@ import org.testng.annotations.Test;
 public class TestClass1 {
 @Test
 public void a1() {
-	System.out.println("Test Class 1");
+	System.out.println("Test Class 11");
 }
 @Test
 public void a2() {
-	System.out.println("Test Class 1");
+	System.out.println("Test Class 11");
 }
 @Test
 public void a3() {
-	System.out.println("Test Class 1");
+	System.out.println("Test Class 11");
 }
 @Test
 public void a4() {
-	System.out.println("Test Class 1");
+	System.out.println("Test Class 11");
 }
 }
