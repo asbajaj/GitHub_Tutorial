2b9489d88a6e94fdc264c5ba0f482863fabadf67
Ashish <ashishbajaj@live.com>
2018-10-04 23:48:27 +0530

    Initial Commit

---
+++ src/test/java/com/testpackage/TestClass1.java
@@ -0,0 +1,21 @@
+package com.testpackage;
+import org.testng.annotations.Test;
+
+public class TestClass1 {
+@Test
+public void a1() {
+	System.out.println("Test Class 1");
+}
+@Test
+public void a2() {
+	System.out.println("Test Class 1");
+}
+@Test
+public void a3() {
+	System.out.println("Test Class 1");
+}
+@Test
+public void a4() {
+	System.out.println("Test Class 1");
+}
+}
---
+++ src/test/java/com/testpackage/TestClass2.java
@@ -0,0 +1,21 @@
+package com.testpackage;
+import org.testng.annotations.Test;
+
+public class TestClass2 {
+	@Test
+	public void a1() {
+		System.out.println("Test Class 2");
+	}
+	@Test
+	public void a2() {
+		System.out.println("Test Class 2");
+	}
+	@Test
+	public void a3() {
+		System.out.println("Test Class 2");
+	}
+	@Test
+	public void a4() {
+		System.out.println("Test Class 2");
+	}
+}
